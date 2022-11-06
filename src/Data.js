8330d87523bdf921b146265959e80f182bcc3295
Chris <chrishethertonweb@gmail.com>
2022-11-06 12:33:45 +0100

    set up navbar

---
+++ src/Data.js
@@ -0,0 +1,10 @@
+const data = [
+    {
+        name: 'Colosseum',
+        location: 'Rome',
+        startDate: '24/10/2021',
+        endDate: '28/10/2021',
+        description: 'Colosseum is one of the 7 Greatest WOnder of the World, worth your time. One of the most visited place on Earth adn a representation of the history of the city',
+        image:'../public/images/Colosseo_2020.jpg'
+    }
+]
---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+import {BiWorld} from 'react-icons/bi'
+
+function Navbar() {
+    
+    
+    return ( 
+          <nav className="navbar">
+            <BiWorld size={'30'}/>
+            <h2>my Travel Journal</h2>
+          </nav>  
+     );
+}
+
+export default Navbar;
