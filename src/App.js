3097868bec6f71b8804d64fe06f4a33dfcc7ffe0
Chris <chrishethertonweb@gmail.com>
2022-11-06 12:41:54 +0100

    added Data and small css to navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import './App.css';
+import './index.css';
 import Navbar from './components/Navbar';
+import Data from './Data'
 
 function App() {
   return (
--- src/Data.js
+++ src/Data.js
@@ -5,6 +5,24 @@ const data = [
         startDate: '24/10/2021',
         endDate: '28/10/2021',
         description: 'Colosseum is one of the 7 Greatest WOnder of the World, worth your time. One of the most visited place on Earth adn a representation of the history of the city',
-        image:'../public/images/Colosseo_2020.jpg'
+        image:'../public/images/Colosseo_2020.jpg',
+    },
+    {
+        name: 'Oktoberfest',
+        location: 'Munich',
+        startDate: '24/9/2021',
+        endDate: '28/9/2021',
+        description: 'Oktoberfest (or Wiesn to locals) is the biggest Folkfest in the whole World. Beer based has also rides and food. Being able to attend it is the most enjoying experence you will ever have',
+        image:'../public/images/oktoberfest-wiesn.jpg'
+    },
+    {
+        name: 'Gyza Pyramid',
+        location: 'Rome',
+        startDate: '21/12/2006',
+        endDate: '28/12/2006',
+        description: 'The Great Pyramid of Giza is the largest Egyptian pyramid and the tomb of Fourth Dynasty pharaoh Khufu. Built in the early 26th century BC during a period of around 27 years, the pyramid is the oldest of the Seven Wonders of the Ancient World, and the only one to remain largely intact',
+        image:'../public/images/gyza.jpeg'
     }
 ]
+
+export default data
